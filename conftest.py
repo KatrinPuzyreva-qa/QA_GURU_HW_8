86f9927e4d528943ada4927084ff010c120f3353
Екатерина <131952002+KatrinPuzyreva-qa@users.noreply.github.com>
2026-02-05 13:26:02 +0400

    Delete conftest.py

--- conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from selene.support.shared import browser
-
-@pytest.fixture(autouse=True)
-def setup_browser():
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
